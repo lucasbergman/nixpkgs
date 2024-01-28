5b76f12684d39eacc50aa45df85ce8a721604f2c
Lucas Bergman <lucas@bergmans.us>
2024-01-28 11:00:28 -0600

    moneydance: enable adding extra JVM flags
    An example use case is that Java/Swing UIs, including Moneydance, are
    often difficult to use on very high-resolution screens but can be fixed by
    adding something like `-Dsun.java2d.uiScale=2` to JVM flags.

--- pkgs/by-name/mo/moneydance/package.nix
+++ pkgs/by-name/mo/moneydance/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchzip, makeWrapper, openjdk21, openjfx21, }:
+{ lib, stdenv, fetchzip, makeWrapper, openjdk21, openjfx21, jvmFlags ? [ ] }:
 let jdk = openjdk21.override { enableJavaFX = true; };
 in stdenv.mkDerivation (finalAttrs: {
   pname = "moneydance";
@@ -20,7 +20,10 @@ in stdenv.mkDerivation (finalAttrs: {
   # N.B. The quotes around the classpath value protect the `*` from shell
   # expansion. The JVM interprets a classpath value like `dir/*` specially,
   # adding every JAR file in `dir` to the classpath.
-  installPhase = ''
+  installPhase = let
+    jvmFlagString = lib.optionalString (jvmFlags != [ ])
+      (''--add-flags "${lib.strings.escapeShellArgs jvmFlags}"'');
+  in ''
     runHook preInstall
 
     mkdir -p $out/libexec $out/bin
@@ -31,6 +34,7 @@ in stdenv.mkDerivation (finalAttrs: {
       --add-flags javafx.swing,javafx.controls,javafx.graphics \
       --add-flags -classpath \
       --add-flags "'${placeholder "out"}/libexec/*'" \
+      ${jvmFlagString} \
       --add-flags Moneydance
 
     runHook postInstall
